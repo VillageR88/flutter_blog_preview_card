b8449d30310683b647894da7969b8828ebd8c27a
VillageR88 <villager88pl@gmail.com>
2024-08-15 16:53:58 +0200

    initial responsive
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -44,146 +44,156 @@ class MyHomePageState extends State<MyHomePage> {
             padding: const EdgeInsets.all(40),
             color: const Color(0xFFF4D04E),
             child: Center(
-              child: AnimatedContainer(
-                duration: const Duration(milliseconds: 150),
-                width: 384,
-                constraints: const BoxConstraints(
-                  minHeight: 522,
-                ),
-                decoration: BoxDecoration(
-                  color: const Color.fromARGB(255, 255, 255, 255),
-                  borderRadius: BorderRadius.circular(10),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black,
-                      blurRadius: 0,
-                      offset: isButtonHovered
-                          ? const Offset(16, 16)
-                          : const Offset(8, 8),
+              child: LayoutBuilder(
+                builder: (context, constraints) {
+                  // Determine the width based on the screen width
+                  double containerWidth =
+                      constraints.maxWidth > 768 ? 384 : 327;
+
+                  return AnimatedContainer(
+                    duration: const Duration(milliseconds: 150),
+                    width: containerWidth,
+                    constraints: const BoxConstraints(
+                      minHeight: 522,
                     ),
-                  ],
-                  border: Border.all(
-                    color: const Color.fromARGB(255, 0, 0, 0),
-                    width: 1,
-                  ),
-                ),
-                padding: const EdgeInsets.all(24),
-                child: Column(
-                  children: [
-                    Align(
-                      alignment: Alignment.topCenter,
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(10),
-                        child: SvgPicture.asset(
-                          'assets/images/illustration-article.svg',
-                          height: 200.0,
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 24),
-                    SizedBox(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Container(
-                            padding: const EdgeInsets.symmetric(
-                                horizontal: 12, vertical: 4),
-                            decoration: BoxDecoration(
-                              color: const Color(0xFFF4D04E),
-                              borderRadius: BorderRadius.circular(4),
-                            ),
-                            child: const Text(
-                              'Learning',
-                              style: TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.w800),
-                            ),
-                          ),
-                          const SizedBox(height: 12),
-                          const Text(
-                            'Published 21 Dec 2023',
-                            style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 14,
-                                fontWeight: FontWeight.w600),
-                          ),
-                          const SizedBox(height: 12),
-                          MouseRegion(
-                            onEnter: (_) =>
-                                setState(() => isButtonHovered = true),
-                            onExit: (_) =>
-                                setState(() => isButtonHovered = false),
-                            child: TextButton(
-                              style: ButtonStyle(
-                                backgroundColor: WidgetStateProperty.all(
-                                  const Color.fromARGB(255, 255, 255, 255),
-                                ),
-                                padding: WidgetStateProperty.all(
-                                  const EdgeInsets.symmetric(
-                                    horizontal: 0,
-                                    vertical: 0,
-                                  ),
-                                ),
-                                overlayColor:
-                                    WidgetStateProperty.all(Colors.transparent),
-                                foregroundColor:
-                                    WidgetStateProperty.resolveWith((states) {
-                                  if (states.contains(WidgetState.hovered)) {
-                                    return const Color(0xFFF4D04E);
-                                  }
-                                  return Colors.black87;
-                                }),
-                                animationDuration: const Duration(
-                                    milliseconds:
-                                        150), // Adjust the duration here
-                              ),
-                              child: const Text(
-                                'HTML & CSS foundations',
-                                style: TextStyle(
-                                  fontSize: 24,
-                                  fontWeight: FontWeight.w800,
-                                ),
-                              ),
-                              onPressed: () {},
-                            ),
-                          ),
-                          const SizedBox(height: 12),
-                          const Text(
-                            'These languages are the backbone of every website, defining structure, content, and presentation.',
-                            style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.w600,
-                                color: Color(0xFF7F7F7F)),
-                          ),
-                        ],
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 24,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Image.asset(
-                          'assets/images/image-avatar.webp',
-                          height: 32.0,
-                          fit: BoxFit.cover,
-                        ),
-                        const SizedBox(width: 12),
-                        const Text(
-                          'Greg Hooper',
-                          style: TextStyle(
-                            fontSize: 14,
-                            fontWeight: FontWeight.w800,
-                          ),
+                    decoration: BoxDecoration(
+                      color: const Color.fromARGB(255, 255, 255, 255),
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black,
+                          blurRadius: 0,
+                          offset: isButtonHovered
+                              ? const Offset(16, 16)
+                              : const Offset(8, 8),
                         ),
                       ],
-                    )
-                  ],
-                ),
+                      border: Border.all(
+                        color: const Color.fromARGB(255, 0, 0, 0),
+                        width: 1,
+                      ),
+                    ),
+                    padding: const EdgeInsets.all(24),
+                    child: Column(
+                      children: [
+                        Align(
+                          alignment: Alignment.topCenter,
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(10),
+                            child: SvgPicture.asset(
+                              'assets/images/illustration-article.svg',
+                              height: 200.0,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 24),
+                        SizedBox(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 12, vertical: 4),
+                                decoration: BoxDecoration(
+                                  color: const Color(0xFFF4D04E),
+                                  borderRadius: BorderRadius.circular(4),
+                                ),
+                                child: const Text(
+                                  'Learning',
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w800),
+                                ),
+                              ),
+                              const SizedBox(height: 12),
+                              const Text(
+                                'Published 21 Dec 2023',
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w600),
+                              ),
+                              const SizedBox(height: 12),
+                              MouseRegion(
+                                onEnter: (_) =>
+                                    setState(() => isButtonHovered = true),
+                                onExit: (_) =>
+                                    setState(() => isButtonHovered = false),
+                                child: TextButton(
+                                  style: ButtonStyle(
+                                    backgroundColor: WidgetStateProperty.all(
+                                      const Color.fromARGB(255, 255, 255, 255),
+                                    ),
+                                    padding: WidgetStateProperty.all(
+                                      const EdgeInsets.symmetric(
+                                        horizontal: 0,
+                                        vertical: 0,
+                                      ),
+                                    ),
+                                    overlayColor: WidgetStateProperty.all(
+                                        Colors.transparent),
+                                    foregroundColor:
+                                        WidgetStateProperty.resolveWith(
+                                            (states) {
+                                      if (states
+                                          .contains(WidgetState.hovered)) {
+                                        return const Color(0xFFF4D04E);
+                                      }
+                                      return Colors.black87;
+                                    }),
+                                    animationDuration: const Duration(
+                                        milliseconds:
+                                            150), // Adjust the duration here
+                                  ),
+                                  child: const Text(
+                                    'HTML & CSS foundations',
+                                    style: TextStyle(
+                                      fontSize: 24,
+                                      fontWeight: FontWeight.w800,
+                                    ),
+                                  ),
+                                  onPressed: () {},
+                                ),
+                              ),
+                              const SizedBox(height: 12),
+                              const Text(
+                                'These languages are the backbone of every website, defining structure, content, and presentation.',
+                                style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w600,
+                                    color: Color(0xFF7F7F7F)),
+                              ),
+                            ],
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 24,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Image.asset(
+                              'assets/images/image-avatar.webp',
+                              height: 32.0,
+                              fit: BoxFit.cover,
+                            ),
+                            const SizedBox(width: 12),
+                            const Text(
+                              'Greg Hooper',
+                              style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w800,
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  );
+                },
               ),
             ),
           ),
